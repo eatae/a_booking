f03e2d9238587938022a6dadd5dcb4bc12e8b1dc
Eatae <al-loco@mail.ru>
2021-01-21 18:57:45 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,4 @@
 
 php composer.phar install
 cd docker
-docker-compose up --build -d &&
-    write "Please wait 10 seconds" &&
-    sleep 10s &&
-    docker exec a_booking-cli php create_tables
+docker-compose up --build -d
